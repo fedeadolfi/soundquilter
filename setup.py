0128b1581a15a05d17dc54d2f68fa6aa792d649c
Fede Adolfi <fedeadolfi@gmail.com>
2020-07-08 11:42:56 +0200

    Add setup file.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Sound-Quilter",
+    version="0.1",
+    author="Federico Adolfi",
+    author_email="fedeadolfi@gmail.com",
+    description="Generalization and implementation of sound quilting algorithms",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "soundfile",
+        ],
+    )
+

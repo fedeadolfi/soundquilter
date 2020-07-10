28a6591b3d529bb52a0fd5e7e6bad6597915c665
Fede Adolfi <fedeadolfi@gmail.com>
2020-07-10 19:16:28 +0200

    Fix package name in setup file.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Sound-Quilter",
+    name="sound_quilter",
     version="0.1",
     author="Federico Adolfi",
     author_email="fedeadolfi@gmail.com",
